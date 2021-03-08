0a420fb27c733864f8a63d27e0d36b09725c12e3
Frank Nong <1205503810@qq.com>
2021-03-08 09:19:26 +0800

    add copy script

---
+++ sync_tcma_client.bat
@@ -0,0 +1,9 @@
+cd /D D:\SoftWork\VB\Project\TCMA 2.0 China-170413\TCMA 2.0 China\TCMA Full\bin\x86\Debug
+set target=C:\Program Files (x86)\HerbMiners Informatics Limited\TCMA
+xcopy /Y Report %target%\Report\
+xcopy /Y Lib %target%\Lib\
+xcopy /Y Sound %target%\Sound\
+xcopy /Y LocationSelector.exe %target%\
+xcopy /Y TCMA.exe %target%\
+xcopy /Y LocationSelector.exe.config %target%\
+rem xcopy /Y TCMA.exe.config %target%\
