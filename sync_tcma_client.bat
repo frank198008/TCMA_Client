83660a67bf07ee8db1bacd31aa8b451a50569ccd
Frank Nong <1205503810@qq.com>
2021-03-12 11:42:10 +0800

    fix bat bug

--- sync_tcma_client.bat
+++ sync_tcma_client.bat
@@ -1,5 +1,5 @@
-cd /D D:\SoftWork\VB\Project\TCMA 2.0 China-170413\TCMA 2.0 China\TCMA Full\bin\x86\Debug
-set target=C:\Program Files (x86)\HerbMiners Informatics Limited\TCMA
+cd /D %~dp0
+set target="C:\Program Files (x86)\HerbMiners Informatics Limited\TCMA"
 xcopy /Y Report %target%\Report\
 xcopy /Y Lib %target%\Lib\
 xcopy /Y Sound %target%\Sound\
@@ -7,3 +7,4 @@ xcopy /Y LocationSelector.exe %target%\
 xcopy /Y TCMA.exe %target%\
 xcopy /Y LocationSelector.exe.config %target%\
 rem xcopy /Y TCMA.exe.config %target%\
+pause
